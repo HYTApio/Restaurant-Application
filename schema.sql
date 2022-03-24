618a8ee748fe898c9450800cc2848ab221bd94e3
HYTApio <101640682+HYTApio@users.noreply.github.com>
2022-03-24 22:29:51 +0200

    add schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    password TEXT,
+    role INTEGER
+);
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    name TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE restaurantinfo (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INTEGER REFERENCES restaurants,
+    info TEXT,
+    openinghours TEXT,
+    address TEXT
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    restaurant_id INTEGER REFERENCES restaurants,
+    stars INTEGER,
+    comment TEXT
+);
