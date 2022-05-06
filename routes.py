63b40caad68d84ed1053c83704e2f4cf79b5aedc
HYTapio <tapio.uzal@helsinki.fi>
2022-05-06 18:48:54 +0300

    changed imports

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, redirect
-import users
-import restaurants
+from users import login, register, require_role, logout, check_csrf, user_id
+from restaurants import remove_restaurant, add_restaurant, get_restaurant_info, search_restaurant, update_restaurant, get_all_restaurants, user_restaurants
 from app import app
+from reviews import add_review, get_restaurant_review
+from foods import add_alacartefood, add_lunchfood, get_alacartefood, get_lunchfood
 
 
 @app.route("/")
@@ -11,11 +13,11 @@ def index():
     Returns:
         Avaa index.html
     """
-    return render_template("index.html", restaurants=restaurants.get_all_restaurants())
+    return render_template("index.html", restaurants=get_all_restaurants())
 
 
 @app.route("/restaurant/<int:restaurant_id>")
-def show_restaurant(restaurant_id):
+def show_restaurant_route(restaurant_id):
     """Ravintolan sivu
 
     Args:
@@ -24,32 +26,32 @@ def show_restaurant(restaurant_id):
     Returns:
         Avaa restaurant.html
     """
-    info = restaurants.get_restaurant_info(restaurant_id)
+    info = get_restaurant_info(restaurant_id)
     if not info:
         return redirect("/")
 
     if info[5] == 0:
         return redirect("/")
 
-    reviews = restaurants.get_restaurant_review(restaurant_id)
-    alacarte = restaurants.get_alacartefood(restaurant_id)
-    lunch = restaurants.get_lunchfood(restaurant_id)
+    reviews = get_restaurant_review(restaurant_id)
+    alacarte = get_alacartefood(restaurant_id)
+    lunch = get_lunchfood(restaurant_id)
     return render_template("restaurant.html", id=restaurant_id, name=info[0], creator=info[1], info=info[2], openinghours=info[3], address=info[4], reviews=reviews, creator_id=info[6], alacarte=alacarte, lunch=lunch)
 
 
 @app.route("/logout")
-def logout():
+def logout_route():
     """Kirjautuminen ulos
 
     Returns:
         Avaa index.html
     """
-    users.logout()
+    logout()
     return redirect("/")
 
 
 @app.route("/register", methods=["get", "post"])
-def register():
+def register_route():
     """Rekisteröitymissivu
 
     Returns:
@@ -76,14 +78,14 @@ def register():
         if role not in ("1", "2"):
             return render_template("error.html", message="Väärä käyttäjärooli")
 
-        if not users.register(username, password1, role):
+        if not register(username, password1, role):
             return render_template("error.html", message="Käyttäjä nimi on jo käytössä")
 
         return redirect("/")
 
 
 @app.route("/login", methods=["get", "post"])
-def login():
+def login_route():
     """Kirjautumis sivu
 
     Returns:
@@ -97,20 +99,20 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if not users.login(username, password):
+        if not login(username, password):
             return render_template("error.html", message="Väärä tunnus tai salasana")
 
         return redirect("/")
 
 
 @app.route("/review", methods=["post"])
-def review():
+def review_route():
     """Arvostelusivu
 
     Returns:
         Avaa ravintolan sivun
     """
-    users.check_csrf()
+    check_csrf()
     restaurant_id = request.form["restaurant_id"]
     stars = int(request.form["stars"])
     if stars < 1 or stars > 5:
@@ -123,34 +125,34 @@ def review():
     if comment == "":
         comment = "-"
 
-    restaurants.add_review(restaurant_id, users.user_id(), stars, comment)
+    add_review(restaurant_id, user_id(), stars, comment)
     return redirect("/restaurant/"+str(restaurant_id))
 
 
 @app.route("/remove", methods=["get", "post"])
-def remove_restaurant():
+def remove_restaurant_route():
     """Ravintolan poisto
 
     Returns:
         Avaa index.html
         Avaa remove.html
     """
-    users.require_role(2)
+    require_role(2)
     if request.method == "GET":
-        user_restaurants = restaurants.user_restaurants(users.user_id())
-        return render_template("remove.html", restaurants=user_restaurants)
+        restaurants = user_restaurants(user_id())
+        return render_template("remove.html", restaurants=restaurants)
 
     if request.method == "POST":
-        users.check_csrf()
+        check_csrf()
         if "restaurant" in request.form:
             restaurant = request.form["restaurant"]
-            restaurants.remove_restaurant(restaurant, users.user_id())
+            remove_restaurant(restaurant, user_id())
 
         return redirect("/")
 
 
 @app.route("/add", methods=["get", "post"])
-def add_restaurant():
+def add_restaurant_route():
     """Ravintolan lisäys sivu
 
     Returns:
@@ -158,12 +160,12 @@ def add_restaurant():
         Avaa add.html
         Avaa error.html
     """
-    users.require_role(2)
+    require_role(2)
     if request.method == "GET":
         return render_template("add.html")
 
     if request.method == "POST":
-        users.check_csrf()
+        check_csrf()
         name = request.form["name"]
         if len(name) > 25:
             return render_template("error.html", message="Ravintolan nimessä saa korkeintaan olla 25 merkkiä")
@@ -194,13 +196,13 @@ def add_restaurant():
         if address == "":
             address = "ei tietoa"
 
-        restaurant_id = restaurants.add_restaurant(
-            name, info, openinghours, address, users.user_id(), searchname)
+        restaurant_id = add_restaurant(
+            name, info, openinghours, address, user_id(), searchname)
         return redirect("/restaurant/"+str(restaurant_id))
 
 
 @app.route("/search", methods=["get"])
-def search():
+def search_route():
     """Etsimis sivu
 
     Returns:
@@ -208,18 +210,18 @@ def search():
     """
     query = request.args['query']
     query_help = query.lower()
-    return render_template("search.html", restaurants=restaurants.search_restaurant(query_help), search=query)
+    return render_template("search.html", restaurants=search_restaurant(query_help), search=query)
 
 
 @app.route("/update_info", methods=["post"])
-def update_info():
+def update_info_route():
     """Päivitys sivu
 
     Returns:
         Avaa error.html
         Avaa ravintolan sivun
     """
-    users.check_csrf()
+    check_csrf()
     name = request.form["name"]
     if len(name) > 25:
         return render_template("error.html", message="Ravintolan nimessä saa korkeintaan olla 25 merkkiä")
@@ -251,20 +253,20 @@ def update_info():
         address = "ei tietoa"
 
     restaurant_id = request.form["restaurant_id"]
-    restaurants.update_restaurant(
-        name, info, openinghours, address, users.user_id(), restaurant_id, searchname)
+    update_restaurant(
+        name, info, openinghours, address, user_id(), restaurant_id, searchname)
     return redirect("/restaurant/"+str(restaurant_id))
 
 
 @app.route("/add_alacartefood", methods=["post"])
-def add_alacartefood():
+def add_alacartefoo_route():
     """Alacarte ruuan lisäys
 
     Returns:
         Avaa error.html
         Avaa ravintolan sivun
     """
-    users.check_csrf()
+    check_csrf()
     name = request.form["name"]
     if len(name) > 25:
         return render_template("error.html", message="Ruuan nimessä saa korkeintaan olla 25 merkkiä")
@@ -283,19 +285,19 @@ def add_alacartefood():
         return render_template("error.html", message="Ruualla pitää olla hinta")
 
     restaurant_id = request.form["restaurant_id"]
-    restaurants.add_alacartefood(name, price, restaurant_id)
+    add_alacartefood(name, price, restaurant_id)
     return redirect("/restaurant/"+str(restaurant_id))
 
 
 @app.route("/add_lunchfood", methods=["post"])
-def add_lunchfood():
+def add_lunchfood_route():
     """Lounas ruuan lisäys
 
     Returns:
         Avaa error.html
         Avaa ravintolan sivun
     """
-    users.check_csrf()
+    check_csrf()
     name = request.form["name"]
     if len(name) > 25:
         return render_template("error.html", message="Ruuan nimessä saa korkeintaan olla 25 merkkiä")
@@ -314,5 +316,5 @@ def add_lunchfood():
         return render_template("error.html", message="Ruualla pitää olla hinta")
 
     restaurant_id = request.form["restaurant_id"]
-    restaurants.add_lunchfood(name, price, restaurant_id)
+    add_lunchfood(name, price, restaurant_id)
     return redirect("/restaurant/"+str(restaurant_id))
